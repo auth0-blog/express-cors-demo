f67c03e0b1e379b8dc9dd2511b8e1b23ea0a91a5
Steve Hobbs <elkdanger@gmail.com>
2019-03-25 14:14:09 +0000

    Added basic Vuetify UI

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,31 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app>
+    <v-toolbar app>
+      <v-toolbar-title class="headline text-uppercase">
+        <span>Express + CORS</span>&nbsp;
+        <span class="font-weight-light">Demo</span>
+      </v-toolbar-title>
+      <v-spacer></v-spacer>
+    </v-toolbar>
+
+    <v-content>
+      <CallAPI/>
+    </v-content>
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import CallAPI from "./components/CallApi";
 
 export default {
-  name: 'app',
+  name: "App",
   components: {
-    HelloWorld
+    CallAPI
+  },
+  data() {
+    return {
+      //
+    };
   }
-}
+};
 </script>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/components/CallApi.vue
@@ -0,0 +1,19 @@
+<template>
+  <v-container>
+    <v-layout>
+      <v-flex xs12>
+        <h1>Call an API</h1>
+        <p>The purpose of this demo app is to show how cross-origin calls can be made to an API running on a different origin.</p>
+        <p>Press the button below to call the API. Without CORS, this call will fail. With CORS correctly set up, a "Hello World" message should be displayed</p>
+        <v-btn color="info">Call API</v-btn>
+      </v-flex>
+    </v-layout>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: "CallAPI"
+};
+</script>
+
