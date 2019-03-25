5eb2f3ef11a951192c2162624d140e557cb09a17
Steve Hobbs <elkdanger@gmail.com>
2019-03-25 14:58:15 +0000

    Added CORS headers

--- server/index.js
+++ server/index.js
@@ -4,6 +4,16 @@ const debug = require("debug")("server");
 const app = express();
 const port = process.env.SERVER_PORT || 3001;
 
+// Add CORS headers
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
 app.get("/api/ping", (req, res) => {
   res.send({
     msg: "Hello, World"
